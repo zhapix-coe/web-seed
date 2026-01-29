4ef26654ed02b0cdec688445d12dc8d17a42b5b0
Aarthigopal12 <aarthi.g@coe.Zhapix.com>
2026-01-29 17:22:12 +0530

    correct the issue

--- code/javascript/functions/3.funcDefaultParams.js
+++ code/javascript/functions/3.funcDefaultParams.js
@@ -6,10 +6,10 @@ const calculateTotal = function (itemCount = 10, unitPrice = 30) {
   let totalPrice = itemCount * unitPrice;
   return totalPrice;
 }
-// let finalPrice1 = calculateTotal(itemCount,unitPrice);
-// console.log('Final Price with param values -->',finalPrice1);
-// let finalPrice2 = calculateTotal(itemCount);
-// console.log('Final Price with item Count only -->',finalPrice2);
+let finalPrice1 = calculateTotal(itemCount,unitPrice);
+ console.log('Final Price with param values -->',finalPrice1);
+ let finalPrice2 = calculateTotal(itemCount);
+ console.log('Final Price with item Count only -->',finalPrice2);
 let finalPrice3 = calculateTotal(itemCount);
 console.log('Final Price with itemCount=undefined -->', finalPrice3);
 
--- code/javascript/loops/while.js
+++ code/javascript/loops/while.js
@@ -1,10 +1,10 @@
 let itemNames = ['Pen', 'Pencil', 'NoteBook'];
-// for(initialization, condition, increment/decrement)
-// for (let index = 0; index < itemNames.length; index++) {
+ //for(initialization, condition, increment/decrement)
+for (let index = 0; index < itemNames.length; index++) {
 
-//     const item = itemNames[index];
-//     console.log('processing the item....',item);        
-// }
+    const item = itemNames[index];
+     console.log('processing the item....',item);        
+ }
 let index = 0;
 let item = '';
 while (index < itemNames.length) {
